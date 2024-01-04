d06a25771a59350d8034a191109bdad4532160eb
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-01-04 20:16:07 +0545

    to find the duplicate in an array

---
+++ Array/q1.cpp
@@ -0,0 +1,15 @@
+//to find the duplicate in an array
+class Solution{
+    public:
+
+bool containsDuplicate(vector<int>&nums){
+    sort(nums.begin(),nums.end());
+    int n=nums.size();
+    for(int i=1;i<n;i++){
+        if(nums[i]==nums[i-1]){
+            return true;
+        }
+    }
+    return false;
+}
+};
