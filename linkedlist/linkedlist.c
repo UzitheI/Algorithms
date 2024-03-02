8345f8b3629318f83a373ff6b103503655f1ce9d
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-02 12:44:40 +0545

    linkedList

---
+++ linkedlist/linkedlist.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+struct node{
+    int info;
+    struct node* next;
+};
+//this is an example of a linked list
+
+//the properties of linked list are:
+/*
+the consecutive node are not stored consecutively in the memory location and can be scattered
+there is no need to shift the nodes in a linked list when addition or deletion 
+the memory of a single node can be allocated dynamically
+a singly linkedlist has a single pointer for the next node and a doubly linked list has 2 pointers for previous and next node
+
+the properties of linked list are:
+creation:the operation is used to create a linked list
+insertion/deletion
+traversing
+searching
+concatenation=done for joining the first linked list to another linked list
+
+*/
+
+
