d762228774eb64113cff44e25a4a03e5abd5e243
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-02 23:38:35 +0545

    finalStack

--- Stack/imp.c
+++ Stack/imp.c
@@ -1,27 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 #define size 5
-
 struct stack{
     int s[size];
     int top;
 }st;
 
 int stfull(){
-    if(st.top>=size-1){
+    if(st.top >= size-1){
         return 1;
     }
     else{
         return 0;
     }
 }
-void push(int item){
-    st.top++;
-    st.s[st.top]=item;
+
+int push(int item){
+    if(stfull){
+        printf("stack is full");
+    }
+    else{
+        st.s[st.top]=item;
+        st.top++;
+    }
 }
+
 int stempty(){
-    if(st.top==-1){
+    if(st.top<=-1){
         return 1;
     }
     else{
@@ -30,14 +35,21 @@ int stempty(){
 }
 
 int pop(){
-if(stempty){
-    print('full');
+    if(stempty){
+        printf("no elements");
+    }
+    else{
+        int item=st.s[st.top];
+        st.top--;
+        return item;
+    }
 }
-else{
-    int item;
-    item=st.s[st.top];
-    st.top--;
-    return (item);
+void display(){
+    for(int i=st.top;i>=0;i--){
+        printf("\t%d",st.s[i]);
+    }
 }
-}
+
+
+
 
